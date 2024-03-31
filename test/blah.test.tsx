09bdf816bfc894b2ddb0dde3465053a45708fb1e
Jake <freejak5520@gmail.com>
2024-03-31 23:20:35 +0900

    eslint 관련 의존성 수정

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { Paginate } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Paginate totalPages={10} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
