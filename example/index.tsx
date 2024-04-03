912552fcb90baed32877669b0cd4764b6bb48fca
Jake <freejak5520@gmail.com>
2024-04-03 23:16:21 +0900

    기본 기능 (#1)
    * 페이지 버튼 생성 로직 작성
    
    * 리팩터링
    
    * 커스텀 컴포넌트 설정
    
    * 기본 스타일 개선
    
    * README.md 기본 틀
    
    * eslint 관련 의존성 수정
    
    * github action 수정
    
    * github workflows 수정
    
    * Add buttons - First, Last, Previous, Next
    
    * example 수정
    
    * README.md 수정
    
    # Conflicts:
    #	README.md

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,42 @@
-import 'react-app-polyfill/ie11';
 import * as React from 'react';
+import 'react-app-polyfill/ie11';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Paginate } from '../.';
 
 const App = () => {
+  const [page, setPage] = React.useState(1);
+
   return (
     <div>
-      <Thing />
+      <h2>Default</h2>
+      <Paginate page={page} totalPages={10} onChange={setPage} />
+
+      <h2>Custom Components</h2>
+      <Paginate
+        page={page}
+        totalPages={10}
+        onChange={setPage}
+        containerComponent={({ children }) => (
+          <div style={{ display: 'flex', justifyContent: 'start', gap: '4px' }}>
+            {children}
+          </div>
+        )}
+        pageButtonComponent={({ children, active, ...props }) => (
+          <div
+            {...props}
+            style={{
+              backgroundColor: active ? 'blue' : 'white',
+              color: active ? 'white' : 'black',
+              cursor: 'pointer',
+              padding: '10px',
+              border: '1px solid black',
+              borderRadius: '5px',
+            }}
+          >
+            {children}
+          </div>
+        )}
+      />
     </div>
   );
 };
