8c73eec2192af70a3a0e112b95bdeeeb30a110dd
Jake <freejak5520@gmail.com>
2024-04-04 20:21:29 +0900

    test: 커스텀 컴포넌트 사용 테스트

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -11,7 +11,30 @@ describe('it', () => {
 
   it('renders with custom component', () => {
     const component = renderer.create(
-      <Paginate totalPages={100} pageSize={20} />
+      <Paginate
+        page={4}
+        totalPages={10}
+        containerComponent={({ children }) => (
+          <div style={{ display: 'flex', justifyContent: 'start', gap: '4px' }}>
+            {children}
+          </div>
+        )}
+        pageButtonComponent={({ children, active, ...props }) => (
+          <div
+            {...props}
+            style={{
+              backgroundColor: active ? 'blue' : 'white',
+              color: active ? 'white' : 'black',
+              cursor: 'pointer',
+              padding: '10px',
+              border: '1px solid black',
+              borderRadius: '5px',
+            }}
+          >
+            {children}
+          </div>
+        )}
+      />
     );
 
     expect(component.toJSON()).toMatchSnapshot();
